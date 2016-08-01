511796ec5b0d88b769ecda103b472b538edcff4b
martinkovar12 <martinkovar12@gmail.com>
2016-08-02 01:15:59 +0200

    adapter pro teamy

--- app/src/main/java/martinkovar12/riddler/TeamsAdapter.java
+++ app/src/main/java/martinkovar12/riddler/TeamsAdapter.java
@@ -2,22 +2,22 @@ package martinkovar12.riddler;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder> implements View.OnClickListener {
-
+public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder> {
     private Context m_context;
     private List<Team> m_teams;
     private LayoutInflater m_inflater;
-    private int m_originalHeight = 0;
+
 
     public TeamsAdapter(Context context, List<Team> teams) {
         m_context = context;
@@ -28,9 +28,7 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder> 
     @Override
     public TeamsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = m_inflater.inflate(R.layout.team_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(v);
-        viewHolder.itemView.setOnClickListener(this);
-        return viewHolder;
+        return new ViewHolder(v);
     }
 
     @Override
@@ -41,9 +39,9 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder> 
         holder.m_positionTextView.setText(String.valueOf(team.getPosition()));
 
         if (team.isOnTurn()) {
-            holder.itemView.setBackgroundColor(Color.LTGRAY);
+            holder.m_startCompoundButton.setEnabled(true);
         } else {
-            holder.itemView.setBackgroundColor(Color.TRANSPARENT);
+            holder.m_startCompoundButton.setEnabled(false);
         }
     }
 
@@ -52,51 +50,72 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder> 
         return m_teams.size();
     }
 
-    @Override
-    public void onClick(final View v) {
-        if (m_originalHeight == 0) {
-            m_originalHeight = v.getHeight();
-        }
-
-        ValueAnimator valueAnimator;
-        if (v.getHeight() < (m_originalHeight + (int) (m_originalHeight * 1.5))) {
-            valueAnimator = ValueAnimator.ofInt(m_originalHeight, m_originalHeight + (int) (m_originalHeight * 1.5));
-        } else {
-            valueAnimator = ValueAnimator.ofInt(m_originalHeight + (int) (m_originalHeight * 1.5), m_originalHeight);
-        }
-        valueAnimator.setDuration(300);
-        valueAnimator.setInterpolator(new LinearInterpolator());
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            public void onAnimationUpdate(ValueAnimator animation) {
-                v.getLayoutParams().height = (Integer) animation.getAnimatedValue();
-                v.requestLayout();
-            }
-        });
-        valueAnimator.start();
-    }
-
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public View m_expander;
+        public int m_expanderOrigHeight;
+        public int m_expanderCurrHeight;
         public TextView m_idTextView;
         public TextView m_positionTextView;
-        public TextView m_onTurnTextView;
+        public CompoundButton m_startCompoundButton;
+        public TextView m_prepTextView;
+        public Button m_prepSkipButton;
+        public TextView m_actTextView;
+        public Button m_actFailButton;
+        public Button m_actSuccButton;
 
         public ViewHolder(View v) {
             super(v);
             m_expander = v.findViewById(R.id.team_item_expander);
+            m_expanderOrigHeight = m_expander.getHeight();
+            m_expanderCurrHeight = m_expander.getHeight();
             m_idTextView = (TextView) v.findViewById(R.id.team_item_id);
             m_positionTextView = (TextView) v.findViewById(R.id.team_item_position);
-            m_onTurnTextView = (TextView) v.findViewById(R.id.team_item_on_turn);
+            m_startCompoundButton = (CompoundButton) v.findViewById(R.id.team_item_start);
+            m_prepTextView = (TextView) v.findViewById(R.id.team_item_preparation);
+            m_prepSkipButton = (Button) v.findViewById(R.id.team_item_preparation_skip);
+            m_actTextView = (TextView) v.findViewById(R.id.team_item_activity);
+            m_actFailButton = (Button) v.findViewById(R.id.team_item_activity_fail);
+            m_actSuccButton = (Button) v.findViewById(R.id.team_item_activity_succes);
+
+            m_startCompoundButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                    if (m_expanderOrigHeight == 0) {
+                        m_expanderOrigHeight = m_expander.getHeight();
+                    }
+
+                    if (checked) {
+                        animate(m_expanderOrigHeight, 2 * m_expanderOrigHeight);
+                    } else {
+                        animate(m_expanderCurrHeight, m_expanderOrigHeight);
+                    }
+                }
+            });
+
+            m_prepSkipButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+
+        private void animate(int oldValue, int newValue) {
+            m_expanderCurrHeight = newValue;
+
+            ValueAnimator animator = ValueAnimator.ofInt(oldValue, newValue);
+            animator.setDuration(300);
+            animator.setInterpolator(new LinearInterpolator());
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    Object value = animation.getAnimatedValue();
+                    if (value instanceof Integer) {
+                        m_expander.getLayoutParams().height = (Integer) value;
+                        m_expander.requestLayout();
+                    }
+                }
+            });
+            animator.start();
         }
     }
-
-//    public static class ClickListener implements View.OnClickListener
-//    {
-//        @Override
-//        public void onClick(View view) {
-//            int itemPosition = mRecyclerView.getChildLayoutPosition(view);
-//            String item = mList.get(itemPosition);
-//            Toast.makeText(mContext, item, Toast.LENGTH_LONG).show();
-//        }
-//    }
 }
