469ea583228a6829b95aa35a3e6a38de5b922684
martinkovar12 <martinkovar12@gmail.com>
2016-08-10 00:37:34 +0200

    parcelable

--- app/src/main/java/martinkovar12/riddler/activity/Riddle.java
+++ app/src/main/java/martinkovar12/riddler/activity/Riddle.java
@@ -1,13 +1,18 @@
 package martinkovar12.riddler.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 
 import martinkovar12.riddler.R;
+import martinkovar12.riddler.Team;
 
 public class Riddle extends AppCompatActivity
 {
 	public static final String ParameterName_Team = "Team";
+
+	private Team m_team;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +20,15 @@ public class Riddle extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_riddle);
 
-		Object o = savedInstanceState.get(ParameterName_Team);
+		Intent intent = getIntent();
+		Parcelable parcelable = intent.getParcelableExtra(ParameterName_Team);
+		if (parcelable instanceof Team)
+		{
+			m_team = (Team) parcelable;
+		}
+		else
+		{
+			throw new IllegalStateException("Team not found.");
+		}
 	}
 }
