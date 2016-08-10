16f0dce24817f0a482a84d077995bb23b457862f
martinkovar12 <martinkovar12@gmail.com>
2016-08-11 00:24:37 +0200

    odstraneni zbytecnych ikon

--- app/src/main/java/martinkovar12/riddler/activity/Riddle.java
+++ app/src/main/java/martinkovar12/riddler/activity/Riddle.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,7 +18,11 @@ public class Riddle extends AppCompatActivity
 	public static final String ParameterName_Team = "Team";
 
 	private Team m_team;
-	private boolean m_preparationInProgress;
+	private TextView m_timerLabelTextView;
+	private CountDownTimerTextView m_timerTextView;
+	private Button m_skipButton;
+	private Button m_successButton;
+	private Button m_failButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -37,30 +43,54 @@ public class Riddle extends AppCompatActivity
 
 		final ImageView riddleTypeImageView = (ImageView) findViewById(R.id.activity_riddle_riddle_type);
 		final TextView riddleTextView = (TextView) findViewById(R.id.activity_riddle_riddle);
-		final TextView timerLabelTextView = (TextView) findViewById(R.id.activity_riddle_timer_label);
-		final CountDownTimerTextView timerTextView = (CountDownTimerTextView) findViewById(R.id.activity_riddle_timer);
-
-		m_preparationInProgress = true;
-
 		riddleTextView.setText(getResources().getText(R.string.riddle));
-		timerLabelTextView.setText(getResources().getText(R.string.preparation));
-		timerTextView.start(15000);
-		timerTextView.setOnFinishedListener(new CountDownTimerTextView.OnFinishedListener()
+
+		m_timerLabelTextView = (TextView) findViewById(R.id.activity_riddle_timer_label);
+		m_timerLabelTextView.setText(getResources().getText(R.string.preparation));
+
+		m_timerTextView = (CountDownTimerTextView) findViewById(R.id.activity_riddle_timer);
+		m_timerTextView.start(15000);
+		m_timerTextView.setOnFinishedListener(new CountDownTimerTextView.OnFinishedListener()
 		{
 			@Override
 			public void onFinished(CountDownTimerTextView countDownTimerTextView)
 			{
-				if (m_preparationInProgress)
-				{
-					m_preparationInProgress = false;
-					timerLabelTextView.setText(getResources().getText(R.string.riddle));
-					timerTextView.start(30000);
-				}
-				else
-				{
-					//TODO
-				}
+				skipPreparation();
 			}
 		});
+
+		m_skipButton = (Button) findViewById(R.id.activity_riddle_skip);
+		m_successButton = (Button) findViewById(R.id.activity_riddle_success);
+		m_failButton = (Button) findViewById(R.id.activity_riddle_fail);
+	}
+
+	public void onClickSkip(View view)
+	{
+		skipPreparation();
+	}
+
+	public void onClickSuccess(View view)
+	{
+	}
+
+	public void onClickFail(View view)
+	{
+	}
+
+	private void skipPreparation()
+	{
+		m_timerLabelTextView.setText(getResources().getText(R.string.riddle));
+		m_timerTextView.start(30000);
+		m_timerTextView.removeOnFinishedListener();
+		m_timerTextView.setOnFinishedListener(new CountDownTimerTextView.OnFinishedListener()
+		{
+			@Override
+			public void onFinished(CountDownTimerTextView countDownTimerTextView)
+			{
+			}
+		});
+		m_skipButton.setVisibility(View.GONE);
+		m_successButton.setVisibility(View.VISIBLE);
+		m_failButton.setVisibility(View.VISIBLE);
 	}
 }
